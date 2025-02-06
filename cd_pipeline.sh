7cc23a6a699a69e5ca39057e8a53b575038968b8
moomtp <moomtp@gmail.com>
2025-02-06 16:02:41 +0800

    bug fix : ci/cd file location

--- cd_pipeline.sh
+++ cd_pipeline.sh
@@ -1,4 +1,4 @@
 # TODO: implement on product_host
-ansible-playbook -i ansible/inventory/staging_host.yml ansible-playbooks/deploy_service.yml
+ansible-playbook -i ansible/inventory/staging_host.yml ansible/ansible-playbooks/deploy_service.yml
 # TODO: test on product_host
 python3 test/test_res_assi.py 192.168.1.106
--- ci_pipeline.sh
+++ ci_pipeline.sh
@@ -1,4 +1,4 @@
 # TODO: implement on staging_host
-ansible-playbook -i ansible/inventory/staging_host.yml ansible-playbooks/deploy_service.yml
+ansible-playbook -i ansible/inventory/staging_host.yml ansible/ansible-playbooks/deploy_service.yml
 # TODO: test on staging_host
 python3 test/test_res_assi.py 10.123.250.15
