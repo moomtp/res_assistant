9016dddaa9706df501d544438f6cfd1fe1b13512
moomtp <moomtp@gmail.com>
2025-02-06 16:24:57 +0800

    bug fix : can't found roles folder error

--- ci_pipeline.sh
+++ ci_pipeline.sh
@@ -1,4 +1,4 @@
 # TODO: implement on staging_host
-ansible-playbook -i ansible/inventory/staging_host.yml ansible/ansible-playbooks/deploy_service.yml
-# TODO: test on staging_host
+ansible-playbook -i ansible/inventory/staging_host.yml ansible/playbooks/deploy_service.yml --roles-path=ansible/roles
+# TODO: test on staging_host's master node
 python3 test/test_res_assi.py 10.123.250.15
