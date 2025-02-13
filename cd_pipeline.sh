549c23fdc6dd5b714bc21d92e7f6df140e5c5bc0
moomtp <moomtp@gmail.com>
2025-02-13 11:23:59 +0800

    update : refine ci/cd pipeline

--- cd_pipeline.sh
+++ cd_pipeline.sh
@@ -1,6 +1,6 @@
-# TODO: implement on product_host
+#  implement on product_host
 cd ansible
 ansible-playbook -i inventory/product_host.yml playbooks/deploy_service.yml
-# TODO: test on product_host's master node
+# test on product_host's master node
 cd ..
 python3 test/test_res_assi.py 192.168.1.106
--- ci_pipeline.sh
+++ ci_pipeline.sh
@@ -1,6 +1,6 @@
-# TODO: implement on staging_host
+# implement on staging_host
 cd ansible
 ansible-playbook -i inventory/staging_host.yml playbooks/deploy_service.yml
-# TODO: test on staging_host's master node
+# test on staging_host's master node
 cd ..
 python3 test/test_res_assi.py 10.123.250.15
