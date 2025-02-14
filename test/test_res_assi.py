b701e160c595ba33c7501a138857834dc6921281
moomtp <moomtp@gmail.com>
2025-02-14 08:37:44 +0800

    update for debugg

--- test/test_res_assi.py
+++ test/test_res_assi.py
@@ -19,8 +19,8 @@ else:
 
 def test_integration():
     # OAuth2 service configuration
-    oauth_server = "http://localhost:3000"
-    api_server = "http://localhost:5000"
+    # oauth_server = "http://localhost:3000"
+    # api_server = "http://localhost:5000"
    
     # staging ip
     # oauth_server = "http://10.123.250.15:32571"
@@ -31,8 +31,8 @@ def test_integration():
     # api_server = "http://192.168.1.106:30050"
 
 
-    # oauth_server = "http://" + ip  + ":30030"
-    # api_server = "http://" + ip  + ":30050"
+    oauth_server = "http://" + ip  + ":30030"
+    api_server = "http://" + ip  + ":30050"
     
     # Step 1: Get OAuth token
     token_response = requests.post(
